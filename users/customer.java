e297d967dd1cb38e1b98712fb59df99f4856b730
Michele Cavaliere <mighele.cavaliere@gmail.com>
2021-10-24 19:00:14 +0100

    Started on users package

---
+++ users/customer.java
@@ -0,0 +1,9 @@
+package users;
+
+public class customer extends user{
+    
+    public void Customer(){
+        
+    }
+}
+
---
+++ users/user.java
@@ -0,0 +1,14 @@
+package users;
+
+public class user {
+    private int id_num;
+    private String full_name;
+    private String email;
+
+    public void User(int id_num, String full_name, String email){
+        this.id_num = id_num;
+        this.full_name = full_name;
+        this.email = email;
+    }
+
+}
