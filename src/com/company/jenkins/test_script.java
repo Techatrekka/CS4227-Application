32eeee275e8d230d3625f861a5f6bbba55e39f01
Mighele Cavaliere <mighele.cavaliere@gmail.com>
2021-10-26 23:44:22 +0100

    Update to jenkinsfile

--- src/com/company/jenkins/test_script.java
+++ src/com/company/jenkins/test_script.java
@@ -1,7 +1,8 @@
 package com.company.jenkins;
 
-public class test_script{
-    public static void main(String args){
-        System.out.println("JenkinsFile is Working!!");
+public class test_script {
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World");
     }
 }
--- src/com/company/users/staff.java
+++
@@ -1,7 +0,0 @@
-package com.company.users;
-
-interface staff extends user{
-    public void orderStock(Date date, String order);
-    public void takeStock(Date date, String stock, int id_num);
-    public void takePayment();
-}
