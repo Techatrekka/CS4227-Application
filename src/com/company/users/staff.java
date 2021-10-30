9d7f2565ca1af507e2619c026d584a848319fd92
Sean <11585008+Techatrekka@users.noreply.github.com>
2021-10-30 15:27:51 +0100

    This is the start of the work on the Django Rest Framework, These are the setup files for django

--- src/com/company/users/staff.java
+++
@@ -1,8 +0,0 @@
-package src.com.company.users;
-import java.util.Date;
-
-interface staff extends user{
-    public void orderStock(Date date, String order);
-    public void takeStock(Date date, String stock, int id_num);
-    public void takePayment();
-}
