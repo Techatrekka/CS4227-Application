c2821294cb45c0576f5642c13c4c5ef3fe44c6f4
Nicole Berty <nicole.berty@hotmail.com>
2021-11-04 23:25:20 +0000

    Create RestaurantTerminal class which will display the system options to the user in the terminal. Add in method to take input from the user to choose what they want to do on the system.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,9 @@
 package com.company;
 
-import com.company.users.User;
-import com.company.users.UserFactory;
-import org.json.JSONObject;
-
 public class Main {
+
     public static void main(String[] args) {
-        UserFactory userFactory = new UserFactory();
-        JSONObject managerDetails = new JSONObject(
-                "{\"userID\":\"1\",\"userType\":\"staff\",\"fullName\":\"John doe\",\"email\":\"johndoe@gmail.com\", \"employeeType\":\"Manager\", \"salary\":\"20000\"}"
-        );
-        User manager = userFactory.createUser(managerDetails);
-        System.out.println(manager);
+        RestaurantTerminal restaurantTerminal = new RestaurantTerminal();
+        restaurantTerminal.run();
     }
 }
