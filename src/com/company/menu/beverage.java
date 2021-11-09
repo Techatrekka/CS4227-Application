3afaee9a75a3b401969ed1cff0ade6044cb0350f
Matt Lucey <18247083@studentmail.ul.ie>
2021-11-09 15:18:39 +0000

    Add menu class with specialMenu and SetMenu subclasses Add menuItem and subclasses populate classes with required sets/gets

--- src/com/company/menu/beverage.java
+++ src/com/company/menu/beverage.java
@@ -1,4 +1,26 @@
 package com.company.menu;
 
-public class beverage {
+public class beverage extends MenuItem {
+    @Override
+    public double getPrice() {
+
+        return 0;
+    }
+
+    @Override
+    public void setPrice(double price) {
+        this.price = price;
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/com/company/menu/specialMenu.java
@@ -0,0 +1,43 @@
+package com.company.menu;
+
+import java.util.ArrayList;
+
+public class specialMenu extends menu{
+
+    @Override
+    public int getMenuID() {
+
+        return menuID;
+    }
+
+    @Override
+    public void setMenuID(int id) {
+        this.menuID = id;
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+
+
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+    @Override
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuList;
+    }
+
+    @Override
+    public void setMenuItems(ArrayList<MenuItem> items) {
+        this.menuList = items;
+
+    }
+
+}
