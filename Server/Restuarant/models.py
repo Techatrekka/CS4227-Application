914d59b81a2e908b4794701f1604d0e5bcc1669b
Sean <saslynch12@gmail.com>
2021-11-12 17:20:06 +0000

    update foreignkeys

--- Server/Restuarant/models.py
+++ Server/Restuarant/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.aggregates import Count
 from django.db.models.base import Model
-from django.db.models.deletion import SET_DEFAULT
 from django.db.models.fields import AutoField, DecimalField
 from django.db.models.fields.related import ForeignKey
 
@@ -13,18 +12,18 @@ class Users(models.Model):
     user_type = models.CharField(max_length=500)
 
 class EmployeeSalary(models.Model):
-    user_id = models.OneToOneField(Users, on_delete=models.SET_DEFAULT, default=0, primary_key=True)
+    user_id = models.OneToOneField(Users, on_delete=models.DO_NOTHING, default=0, primary_key=True)
     employee_type = models.CharField(max_length=50)
     salary = models.DecimalField(max_digits=5,decimal_places=2)
 
 class Loyalty(models.Model):
     loyalty_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Users, default=1,on_delete=models.SET_DEFAULT)
+    user_id = models.ForeignKey(Users, default=1,on_delete=models.DO_NOTHING)
     loyalty_points = models.IntegerField()
 
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Users, default=1,on_delete=models.SET_DEFAULT)
+    user_id = models.ForeignKey(Users, default=1,on_delete=models.DO_NOTHING)
     total_cost = models.DecimalField(max_digits=10,decimal_places=10)
 
 class Dishes(models.Model):
@@ -65,15 +64,15 @@ class DrinkItems(models.Model):
 class MenuItem(models.Model):
     menu_item = models.AutoField(primary_key=True)
     Dish_Bev_id = models.IntegerField()
-    menu_id = models.ForeignKey(Menu,default=1,on_delete=models.SET_DEFAULT)
+    menu_id = models.ForeignKey(Menu,default=1,on_delete=models.DO_NOTHING)
     food = models.BooleanField()
 
 class OrderLineItems(models.Model):
     OrderLineItems = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(Orders,default=1,on_delete=models.SET_DEFAULT)
-    menu_item = models.ForeignKey(MenuItem,default=1,on_delete=models.SET_DEFAULT)
+    order_id = models.ForeignKey(Orders,default=1,on_delete=models.DO_NOTHING)
+    menu_item = models.ForeignKey(MenuItem,default=1,on_delete=models.DO_NOTHING)
     food = models.BooleanField()
 
 class Dishes_FoodItems(models.Model):
-    food_id = models.ForeignKey(FoodItems,default=1,on_delete=models.SET_DEFAULT)
-    dish_id = models.ForeignKey(Dishes,default=1,on_delete=models.SET_DEFAULT)
+    food_id = models.ForeignKey(FoodItems,default=1,on_delete=models.DO_NOTHING)
+    dish_id = models.ForeignKey(Dishes,default=1,on_delete=models.DO_NOTHING)
