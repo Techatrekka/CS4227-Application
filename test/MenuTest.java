672c3481d07fd3335ba18e449a432da55e94e9bf
ZedLepplin <43820292+ZedLepplin@users.noreply.github.com>
2021-11-22 19:41:37 +0000

    Add MenuTest Add UserTest

---
+++ test/MenuTest.java
@@ -0,0 +1,50 @@
+import com.company.menu.Menu;
+import com.company.menu.MenuFactory;
+import com.company.restaurant.Database;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.company.menu.Menu;
+import com.company.menu.MenuItem;
+import com.company.menu.MenuFactory;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+class MenuTest {
+    String name = "test name";
+    double price = 10.50;
+    String description = "This is a test";
+    JSONObject menuObj = new JSONObject();
+    ArrayList<Menu> menus = new ArrayList<Menu>();
+    MenuFactory menuFactory = new MenuFactory();
+
+    @Test
+    public void testMenu() {
+        menuObj.put("set_menu_price", price);
+        menuObj.put("name", name);
+        menuObj.put("description", description);
+        menuObj.put("discount", "0.0");
+        menuObj.put("date_created", LocalDate.now());
+
+        int id = Database.writeToTable("menu", menuObj);
+        System.out.println(id);
+        ArrayList<String> cols = new ArrayList<>();
+        cols.add("name");
+        cols.add("set_menu_price");
+        cols.add("description");
+        cols.add("discount");
+        cols.add("date_created");
+        cols.add("menu_id");
+
+        JSONObject menuItemDetails = Database.readFromTable("menu", id, cols, "menu_id", -1, "");
+        Menu menuTest = menuFactory.createMenu(menuItemDetails);
+        System.out.println(menuTest);
+
+        System.out.println("test");
+        assertEquals(name, menuTest.getName());
+        Database.deleteFromTable("menu", "menu_id", menuItemDetails.getInt("menu_id"));
+    }
+}
---
+++ test/UserTest.java
@@ -0,0 +1,36 @@
+import com.company.restaurant.Database;
+import com.company.users.Customer;
+import com.company.users.User;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTest {
+    String name = "test name";
+    String email = "emailtest@email.com";
+    Customer testUser = new Customer();
+    private User user;
+    @Test
+    public void testName(){
+        testUser.setFullName(name);
+        assertEquals(testUser.getFullName(), name);
+    }
+    @Test
+    public void testEmail(){
+        JSONObject userDetails = new JSONObject();
+        userDetails.put("fullname", name);
+        userDetails.put("email", email);
+        userDetails.put("password", "password");
+        userDetails.put("user_type", "customer");
+
+        Database.writeToTable("user", userDetails);
+        JSONObject user = Database.readFromUserTable(email, null);
+        User.createUser(true, email);
+        System.out.println( user.getString("email"));
+        assertEquals(email, user.getString("email"));
+        Database.deleteFromTable("user", "user_id", user.getInt("user_id"));
+    }
+}
