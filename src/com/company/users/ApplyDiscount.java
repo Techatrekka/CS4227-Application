ae1d7f92220aebe038d2922c40fca10e4e5a375e
Nicole Berty <nicole.berty@gmail.com>
2022-04-04 23:45:56 +0100

    Refactor ApplyDiscount class

--- src/com/company/users/ApplyDiscount.java
+++ src/com/company/users/ApplyDiscount.java
@@ -1,11 +1,10 @@
 package com.company.users;
 
 public class ApplyDiscount implements LoyaltyStrategy{
-    private LoyaltyPoints loyaltyPoints;
 
     @Override
     public double applyLoyaltyDiscount(int userID, double price){
-        loyaltyPoints = new LoyaltyPoints(0);
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(0);
         return loyaltyPoints.getLoyaltyPoints(userID)*.01;
     }
 }
